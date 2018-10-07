788ad8166a3c3a1c37acea5f384b5faae8a19920
calvinlfer <calvin.l.fer@gmail.com>
2018-10-07 02:19:20 -0400

    Extract has the Applicative capability

--- src/main/scala/com/workflow/etl/Extract.scala
+++ src/main/scala/com/workflow/etl/Extract.scala
@@ -1,15 +1,26 @@
 package com.workflow.etl
 
-import cats.Functor
+import cats.Applicative
 
 trait Extract[+A] {
   def produce: A
 }
 
 object Extract {
-  def apply[A](a: A): Extract[A] = new Extract[A] {
-    override def produce: A = a
+  implicit val applicativeForExtract: Applicative[Extract] = new Applicative[Extract] {
+    override def pure[A](a: A): Extract[A] = new Extract[A] {
+      override def produce: A = a
+    }
+    override def ap[A, B](fab: Extract[A => B])(fa: Extract[A]): Extract[B] = new Extract[B] {
+      override def produce: B = {
+        val a = fa.produce
+        val `a->b` = fab.produce
+        `a->b`(a)
+      }
+    }
   }
+
+  def apply[A](a: A): Extract[A] = Applicative[Extract].pure[A](a)
 
   implicit class ExtractOps[A](e: Extract[A]) {
     def via[B](t: Transform[A, B]): Extract[B] = new Extract[B] {
@@ -20,6 +31,8 @@ object Extract {
       }
     }
 
+    def >>[B](t: Transform[A, B]): Extract[B] = via(t)
+
     def to[AStatus](l: Load[A, AStatus]): ETLPipeline[AStatus] = new ETLPipeline[AStatus] {
       override def execute(): AStatus = {
         val a = e.produce
@@ -28,16 +41,10 @@ object Extract {
       }
     }
 
-    def zip[BConfig, B, CConfig, C](e1: Extract[B]): Extract[(A, B)] =
-      new Extract[(A, B)] {
-        override def produce: (A, B) = (e.produce, e1.produce)
-      }
-  }
+    def >>>[AStatus](l: Load[A, AStatus]): ETLPipeline[AStatus] = to(l)
 
-  implicit val functorForExtract: Functor[Extract] = new Functor[Extract] {
-    override def map[A, B](fa: Extract[A])(f: A => B): Extract[B] =
-      new Extract[B] {
-        override def produce: B = f(fa.produce)
-      }
+    def zip[BConfig, B, CConfig, C](e1: Extract[B]): Extract[(A, B)] = Applicative[Extract].map2(e, e1)(Tuple2(_, _))
+
+    def <*>[BConfig, B, CConfig, C](e1: Extract[B]): Extract[(A, B)] = zip(e1)
   }
 }
